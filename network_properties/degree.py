dd682ac147d92349e11724febe699c2abf98478a
sdimig <sdimig@netapp.com>
2019-02-09 13:01:10 -0500

    Fixes.

--- network_properties/degree.py
+++ network_properties/degree.py
@@ -125,5 +125,5 @@ if __name__ == '__main__':
 			print("Writing distribution to file " + gx + ".csv")
 			distrib.toPandas().to_csv(gx + ".csv")
 
-			distrib.select('count').show()
+			distrib.select('count').map(lambda x: x).collect()
 			fit = powerlaw.Fit(distrib['count'])
