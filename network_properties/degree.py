b656e6eda898639e94f422ba45fa43624a8d7da2
sdimig <sdimig@netapp.com>
2019-02-09 09:56:58 -0500

    Fixes.

--- network_properties/degree.py
+++ network_properties/degree.py
@@ -58,8 +58,7 @@ def readFile(filename, large, sc, sqlContext):
 	# YOUR CODE HERE
 	v  = lines.filter(lambda line: line.split(delim)[0].isdigit() and line.split(delim)[1].isdigit()).\
 		flatMap(lambda line: line.split(delim)).\
-		map(lambda x: int(x)).\
-		distinct()
+		map(lambda x: int(x))
 	vdf = sqlContext.createDataFrame(v, vschema)
 	vdf.show()
 
