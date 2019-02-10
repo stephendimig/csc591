1009a01ef8cb14eb9fe11c214b6964dde5e691b3
sdimig <sdimig@netapp.com>
2019-02-10 08:25:19 -0500

    Fixes.

--- network_properties/centrality.py
+++ network_properties/centrality.py
@@ -4,16 +4,14 @@ from pyspark.sql import functions
 from graphframes import *
 from pyspark.sql.functions import explode
 
-sc=SparkContext("local", "degree.py")
-sqlContext = SQLContext(sc)
-
-def closeness(g):
-	
+def closeness(g, sc, sqlContext):
 	# Get list of vertices. We'll generate all the shortest paths at
 	# once using this list.
 	# YOUR CODE HERE
+	vertices = g.vertices
 
 	# first get all the path lengths.
+
 
 	# Break up the map and group by ID for summing
 
@@ -21,23 +19,26 @@ def closeness(g):
 
 	# Get the inverses and generate desired dataframe.
 
+if __name__ == '__main__':
+	sc = SparkContext("local", "degree.py")
+	sqlContext = SQLContext(sc)
 
-print("Reading in graph for problem 2.")
-graph = sc.parallelize([('A','B'),('A','C'),('A','D'),
-	('B','A'),('B','C'),('B','D'),('B','E'),
-	('C','A'),('C','B'),('C','D'),('C','F'),('C','H'),
-	('D','A'),('D','B'),('D','C'),('D','E'),('D','F'),('D','G'),
-	('E','B'),('E','D'),('E','F'),('E','G'),
-	('F','C'),('F','D'),('F','E'),('F','G'),('F','H'),
-	('G','D'),('G','E'),('G','F'),
-	('H','C'),('H','F'),('H','I'),
-	('I','H'),('I','J'),
-	('J','I')])
+	print("Reading in graph for problem 2.")
+	graph = sc.parallelize([('A','B'),('A','C'),('A','D'),
+		('B','A'),('B','C'),('B','D'),('B','E'),
+		('C','A'),('C','B'),('C','D'),('C','F'),('C','H'),
+		('D','A'),('D','B'),('D','C'),('D','E'),('D','F'),('D','G'),
+		('E','B'),('E','D'),('E','F'),('E','G'),
+		('F','C'),('F','D'),('F','E'),('F','G'),('F','H'),
+		('G','D'),('G','E'),('G','F'),
+		('H','C'),('H','F'),('H','I'),
+		('I','H'),('I','J'),
+		('J','I')])
+
+	e = sqlContext.createDataFrame(graph,['src','dst'])
+	v = e.selectExpr('src as id').unionAll(e.selectExpr('dst as id')).distinct()
+	print("Generating GraphFrame.")
+	g = GraphFrame(v,e)
 	
-e = sqlContext.createDataFrame(graph,['src','dst'])
-v = e.selectExpr('src as id').unionAll(e.selectExpr('dst as id')).distinct()
-print("Generating GraphFrame.")
-g = GraphFrame(v,e)
-
-print("Calculating closeness.")
-closeness(g).sort('closeness',ascending=False).show()
+	print("Calculating closeness.")
+	closeness(g).sort('closeness',ascending=False).show()
